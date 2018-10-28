1275aa2abb93c584fbc228cf9b4065fbca44f428
user1 <user1@MacBook-Air-Admin.local>
2018-10-29 01:37:08 +0200

    Add POST /users/login route

--- server/server.js
+++ server/server.js
@@ -55,10 +55,6 @@ app.get('/todos/:id', (req, res) => {
 
 });
 
-app.listen(port, () => {
-    console.log(`Started on port ${port}`);
-});
-
 app.delete('/todos/:id', (req, res) => {
     var id = req.params.id;
     console.log(id);
@@ -115,11 +111,29 @@ app.post('/users', (req, res) => {
     }).catch((e) => {
         res.status(400).send(e);
     });
-
 });
 
 app.get('/users/me', authenticate, (req, res) => {
     res.send(req.user);
+});
+
+
+app.post('/users/login', (req, res) => {
+
+    var body = _.pick(req.body, ['email', 'password']);
+    
+    User.findByCredentials(body.email, body.password).then((user) => {
+        user.generateAuthToken().then((token) => {
+            res.header('x-auth', token).send(user);
+        });
+    }).catch((e) => {
+        res.send(400).send();
+    });
+});
+
+
+app.listen(port, () => {
+    console.log(`Started on port ${port}`);
 });
 
 module.exports = {app};
